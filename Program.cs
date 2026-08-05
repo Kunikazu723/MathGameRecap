095882324d927341fc1ec3a31aca3a96c1678b0a
Kunikazu723 <hyankunikazu@gmail.com>
2026-08-05 16:07:14 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MathGameRecap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
