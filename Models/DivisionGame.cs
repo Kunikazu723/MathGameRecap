9d606b801961617d75a884c98a409bcfa7c6727d
Kunikazu723 <hyankunikazu@gmail.com>
2026-08-07 00:45:44 -0300

    Division GenerateOpernds falls into a special case where not all denominators are possible.

--- Models/DivisionGame.cs
+++ Models/DivisionGame.cs
@@ -4,6 +4,30 @@
     {
         protected override Enums.GameType Type => Enums.GameType.Divide;
 
+        protected override (int, int) GenerateOperands()
+        {
+            int a = Helpers.Rng.Next(0, 100);
+            int b = GenerateDenominator(a);
+
+            return (a, b);
+        }
+
         protected override int PerformOperation(int a, int b) => a / b;
+
+        private int GenerateDenominator(int a)
+        {
+            var availableDenominators = new List<int>();
+            for (int i = 1; i <= a; i++) 
+            {
+                if (a % i == 0)
+                {
+                    availableDenominators.Add(i);
+                }
+            }
+
+            int randomIndex = Helpers.Rng.Next(availableDenominators.Count);
+
+            return availableDenominators[randomIndex];
+        }
     }
 }
