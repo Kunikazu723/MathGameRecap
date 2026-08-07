71f325061a6a476c8e4b59a2ca779a2032a90878
Kunikazu723 <hyankunikazu@gmail.com>
2026-08-07 00:11:46 -0300

    Implementing polymorphic method to generate an answer for to be used at super.RunGame method

--- Models/DivisionGame.cs
+++ Models/DivisionGame.cs
@@ -7,11 +7,10 @@ using System.Threading.Tasks;
 
 namespace MathGameRecap.Models
 {
-    internal class DivisionGame :BaseGame, IGame
+    internal class DivisionGame : BaseGame, IGame
     {
-        public void RunGame()
-        {
-            throw new NotImplementedException();
-        }
+        protected override Enums.GameType Type => Enums.GameType.Divide;
+
+        protected override int PerformOperation(int a, int b) => a / b;
     }
 }
--- Models/MultiplicationGame.cs
+++ Models/MultiplicationGame.cs
@@ -9,9 +9,8 @@ namespace MathGameRecap.Models
 {
     internal class MultiplicationGame : BaseGame, IGame
     {
-        public void RunGame()
-        {
-            throw new NotImplementedException();
-        }
+        protected override Enums.GameType Type => Enums.GameType.Multiply;
+
+        protected override int PerformOperation(int a, int b) => a * b;
     }
 }
