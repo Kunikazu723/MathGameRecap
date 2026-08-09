6a0f2af3041b83b38f2a09ab3294a76a6358bc1d
Kunikazu723 <hyankunikazu@gmail.com>
2026-08-09 16:21:59 -0300

    Organizing requirement comments

--- Program.cs
+++ Program.cs
@@ -17,13 +17,12 @@ namespace MathGameRecap
 
             // TODO - Try to implement levels of difficulty.
 
-            // TODO - Add a timer to track how long the user takes to finish the game.
-
-            // TODO - Create a 'Random Game' option where the players will be presented with questions from random operations
-
             // TODO - To follow the DRY Principle, try using just one method for all games. Additionally, double check your project and try to find
             //opportunities to achieve the same functionality with less code, avoiding repetition when possible.
 
+
+            // DONE - Add a timer to track how long the user takes to finish the game.
+            // DONE - Create a 'Random Game' option where the players will be presented with questions from random operations 
         }
     }
 }
