7ab0ceab4252e92c044ab4cc6a3598fbc6c33cfe
Kunikazu723 <hyankunikazu@gmail.com>
2026-08-12 15:44:34 -0300

    Final touches

--- Program.cs
+++ Program.cs
@@ -12,17 +12,6 @@ namespace MathGameRecap
             var gameEngine = new GameEngine();
             var userInterface = new UserInterface(gameEngine);
             userInterface.MainMenu();
-
-            // Challenges
-
-            // TODO - Try to implement levels of difficulty.
-
-            // TODO - To follow the DRY Principle, try using just one method for all games. Additionally, double check your project and try to find
-            //opportunities to achieve the same functionality with less code, avoiding repetition when possible.
-
-
-            // DONE - Add a timer to track how long the user takes to finish the game.
-            // DONE - Create a 'Random Game' option where the players will be presented with questions from random operations 
         }
     }
 }
